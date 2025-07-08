7971fb4911eaa0beef5b44f51764cc59228c3a2c
eastoye <120681767+eastoye@users.noreply.github.com>
2025-07-08 19:44:25 +0100

    Updated style.css

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
-        login: 'login.html',
         'add-word': 'add-word.html',
         practice: 'practice.html',
         stats: 'stats.html'
