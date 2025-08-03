4cf50bfe4702f743a22a55a22bc58fb69396ceb5
eastoye <120681767+eastoye@users.noreply.github.com>
2025-08-03 19:28:08 +0100

    Fix Firebase storage bucket configuration

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -6,9 +6,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyCaqb2xVon5Lq90Z5eB1OcteZ_OjJy1cW8",
   authDomain: "lexcelerate-35eba.firebaseapp.com",
   projectId: "lexcelerate-35eba",
-  storageBucket: "lexcelerate-35eba.appspot.com",
+  storageBucket: "lexcelerate-35eba.firebasestorage.app",
   messagingSenderId: "352872440845",
-  appId: "1:352872440845:web:3531762a2cc65441e9a144"
+  appId: "1:352872440845:web:3531762a2cc65441e9a144",
+  measurementId: "G-FKTS29MLMS"
 };
 
 // Initialize Firebase
